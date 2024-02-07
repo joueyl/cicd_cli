314f65511c31d1972878ac75bc4742541b25103e
董志斌 <dzb08042@163.com>
2024-02-07 17:10:19 +0800

    fix:run number

--- src/common/selectRun.ts
+++ src/common/selectRun.ts
@@ -1,22 +1,26 @@
 import runSSH from '../option/runSSH'
 import runFTP from '../option/runFTP'
 import { countRunNUM } from './count'
-import { countSuccessNUM } from './count'
+import { countSuccessNUM, countErrorNUM } from './count'
 import type{Config} from '../../types/config'
 export default async function selectRun(config:Config){
     countRunNUM()
     switch(config.serverType){
         case 'ssh':
             runSSH(config).then(() => {
+                countSuccessNUM()
                 process.exit(0)
             }).catch((error) => {
+                countErrorNUM()
                 process.exit(1)
             })
             break
         case 'ftp':
           await runFTP(config).then(() => {
+              countSuccessNUM()
               process.exit(0)
           }).catch((error) => {
+              countErrorNUM()
               process.exit(1)
           })
             break
